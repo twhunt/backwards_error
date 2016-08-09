628d9305d124f7514879f55a7df6625febc41f89
Thomas Hunt <thomashunt13@gmail.com>
2016-08-08 19:05:26 -0700

    Exact SVD complete. Ready for error analysis.

--- back_err2.py
+++ back_err2.py
@@ -1,68 +1,87 @@
-import array
-
-class matrix(object):
-
-    def __init__(self, nrows, ncols, copy_from=None):
-        self.nrows = nrows
-        self.ncols = ncols
-
-        self.num_elts = nrows*ncols
-
-        self.arr = array.array(self.type)
-
-        k = 0
-        if copy_from is not None:
-            while k < copy_from.num_elts:
-                k += 1
-                self.arr.append(copy_from[k])
-        else:
-            while k < self.num_elts:
-                k += 1
-                self.arr.append(0.0)
-
-    def type_code(self):
-        return self.arr.typecode
-
-    def copy(self):
-        this_copy = array.array(self.type)
-        for elt in self.arr:
-            this_copy.append(elt)
-        return this_copy
-
-    def offset(self, i, j):
-        return self.col_offset(j) + i
-
-    def col_offset(self, j):
-        return j*self.nrows
-
-    @staticmethod
-    def LUsolve(P, LU, b):
-
-
-    @staticmethod
-    def forward_solve(LU, b):
-        # Operate on L portion of LU matrix
-        # L can be formed by copying all subdiagonal elements of LU, and placing 1 on the diagonal
-
-        y = matrix(b.nrows, 1, b.type_code())
-
-        y[0] = b[0]
-        r = 1
-        while r != b.nrows:
-            y[r] = b[r]
-            rr = 0
-            while rr != r:
-                y[r] -=
-
+import numpy.linalg
+import scipy.linalg
+import sympy
 
 
 def main():
 
-    nrows = 2
-    ncols = 2
+    num_angles = 128
+    angle_factor = 2.0*numpy.pi/num_angles
 
-    mat = matrix(nrows, ncols, 'd')
+    rhs_angles = [k*angle_factor for k in range(num_angles)]
+    rhss = [numpy.array([[numpy.cos(angle)], [numpy.sin(angle)]]) for angle in rhs_angles]
+
+    U_angle = numpy.pi/3.0
+    U_trig = [numpy.cos(U_angle), numpy.sin(U_angle)]
+    V_angle = numpy.pi/4.0
+    V_trig = [numpy.cos(V_angle), numpy.sin(V_angle)]
+
+    U = rotation_matrix(*U_trig)
+    V = rotation_matrix(*V_trig)
+    singular_values = [1e2, 1e-6]
+    S = numpy.diagflat(singular_values)
+
+    A = numpy.dot(U, S)
+    A = numpy.dot(A, V.T)
+
+    LU, P = scipy.linalg.lu_factor(A)
+
+    solns = [scipy.linalg.lu_solve((LU, P), rhs) for rhs in rhss]
+
+
+    rational_A = exact_A(A)
+    U, D, V = exact_svd(rational_A)
     pass
+
+def rotation_matrix(cosine, sine):
+    return numpy.array([[cosine, -sine], [sine, cosine]])
+
+def exact_A(A):
+
+    m = len(A)
+    n = len(A[0])
+
+    out_A = sympy.zeros(m, n)
+
+    for r in range(m):
+        for c in range(n):
+            out_A[r, c] = sympy.Rational(A[r, c])
+
+    return out_A
+
+def exact_svd(A):
+
+
+    U, sing_vals_sqrd_U = singular_vecs(A*A.T)
+    V, sing_vals_sqrd_V = singular_vecs(A.T*A)
+
+    sing_vals = [sympy.sqrt(sing_vals_sqrd) for sing_vals_sqrd in sing_vals_sqrd_U]
+
+    D = sympy.zeros(2)
+    for k in range(2):
+        D[k, k] = sing_vals[k]
+
+    return U, D, V
+
+def singular_vecs(A_prod):
+
+    r1 = A_prod.eigenvects()
+
+    svals_sqrd = [r1[i][0] for i in range(2)]
+
+    perm = [0, 1] if svals_sqrd[0] >= svals_sqrd[1] else [1, 0]
+
+    svals_sqrd = [svals_sqrd[i] for i in perm]
+
+    svecs = [r1[i][2][0] for i in range(2)]
+    svecs = [evec/evec.norm() for evec in svecs]
+
+    svecs = [svecs[i] for i in perm]
+
+    U = sympy.Matrix([list(svecs[0]), list(svecs[1])])
+    U = U.T
+
+    return U, svals_sqrd
 
 if __name__ == '__main__':
     main()
